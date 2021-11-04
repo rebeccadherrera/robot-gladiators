f2fc6f24796e0da0421317693b239df4514bdac6
Rebecca Herrera <rebeccadherrera@gmail.com>
2021-11-03 21:19:58 -0400

    first look at javascript

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,7 @@
-window.alert("This is an alert! JavaScript is running!");
+window.prompt("What is your robot's name?");
+
+function fight () {
+    window.alert("The fight has begun!");
+}
+
+fight();
