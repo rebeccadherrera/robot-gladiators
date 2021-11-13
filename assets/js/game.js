26549a9cdf4d70adc8162047ca8e9402c4ac023b
Rebecca Herrera <rebeccadherrera@gmail.com>
2021-11-13 16:36:24 -0500

    bug changes for fight/skip

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,6 +7,37 @@ var randomNumber = function(min, max) {
   return value;
 };
 
+var fightOrSkip = function() {
+  //repeat and execute as long as the enemy-robot is alive
+  while (playerInfo.health > 0 && enemy.health > 0) {
+    //ask player if they'd like to fight or skip using fightOrSkip function
+    if (fightOrSkip()) {
+      //if true, leave fight by breaking loop
+      break;
+    }
+
+    promptFight = promptFight.toLowerCase();
+
+  // if player picks "skip" confirm and then stop the loop
+  if (promptFight === "skip" || promptFight === "SKIP") {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+      window.alert(playerInfo.name + " has decided to skip this fight. Goodbye!");
+      // subtract money from playerMoney for skipping, but don't let them go into the negative
+      playerInfo.money = Math.max(0, playerInfo.money - 10);
+
+      //return true if player wants to leave
+      return true;
+
+      return false;
+    }
+  }
+}
+};
+
 // fight function (now with parameter for enemy's object holding name, health, and attack values)
 var fight = function(enemy) {
   while (playerInfo.health > 0 && enemy.health > 0) {
@@ -67,7 +98,7 @@ var fight = function(enemy) {
       window.alert(playerInfo.name + ' still has ' + playerInfo.health + ' health left.');
     }
   }
-};
+}
 
 // function to start a new game
 var startGame = function() {
